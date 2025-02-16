b358d4ba126883b30c19a566b27ff0ac9948fc96
weijunext <weijunext@gmail.com>
2025-02-16 11:04:37 +0800

    feat: add responsive mobile menu with dropdown for locale and theme settings

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,10 +1,22 @@
 import LocaleSwitcher from "@/components/LocaleSwitcher";
 import { ThemeToggle } from "@/components/ThemeToggle";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { siteConfig } from "@/config/site";
+import { Menu } from "lucide-react";
+import { useTranslations } from "next-intl";
 import Image from "next/image";
 import Link from "next/link";
 
 const Header = () => {
+  const t = useTranslations("Home");
   return (
     <header className="py-4">
       <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-6">
@@ -19,15 +31,56 @@ const Header = () => {
                 height={32}
               />
               <span className="text-gray-800 dark:text-gray-200">
-                Next.js i18n Starter
+                {t("title")}
               </span>
             </Link>
             <div className="hidden md:flex md:gap-x-6"></div>
           </div>
 
           <div className="flex items-center gap-x-2 md:gap-x-4 lg:gap-x-6">
-            <LocaleSwitcher />
-            <ThemeToggle />
+            {/* PC */}
+            <div className="hidden md:flex items-center gap-x-4">
+              <LocaleSwitcher />
+              <ThemeToggle />
+            </div>
+
+            {/* Mobile */}
+            <div className="md:hidden">
+              <DropdownMenu>
+                <DropdownMenuTrigger className="p-2">
+                  <Menu className="h-5 w-5" />
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>
+                    {" "}
+                    <Link
+                      href="/"
+                      className="flex items-center space-x-1 font-bold"
+                    >
+                      <Image
+                        alt={siteConfig.name}
+                        src="/logo.svg"
+                        className="w-6 h-6"
+                        width={32}
+                        height={32}
+                      />
+                      <span className="text-gray-800 dark:text-gray-200">
+                        {t("title")}
+                      </span>
+                    </Link>
+                  </DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuGroup>
+                    <DropdownMenuItem className="p-2 focus:bg-transparent justify-end">
+                      <div className="flex items-center gap-x-4">
+                        <LocaleSwitcher />
+                        <ThemeToggle />
+                      </div>
+                    </DropdownMenuItem>
+                  </DropdownMenuGroup>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
           </div>
         </nav>
       </div>
