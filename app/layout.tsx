e571b8b32e1ec8a56cc55afc27dd0d40afa3ae32
weijunext <weijunext@gmail.com>
2025-02-16 23:13:54 +0800

    feat: integrate Google AdSense into layout with other analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import BaiDuAnalytics from "@/app/BaiDuAnalytics";
+import GoogleAdsense from "@/app/GoogleAdsense";
 import GoogleAnalytics from "@/app/GoogleAnalytics";
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/header/Header";
@@ -64,8 +65,9 @@ export default async function RootLayout({
           <></>
         ) : (
           <>
-            <GoogleAnalytics />
             <BaiDuAnalytics />
+            <GoogleAnalytics />
+            <GoogleAdsense />
           </>
         )}
       </body>
