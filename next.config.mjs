a299279cfb8237b1143402e4f6c4d85e5864b54d
weijunext <weijunext@gmail.com>
2025-03-02 21:22:32 +0800

    chore: configure Next.js compiler to remove console logs except errors

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,12 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  compiler: {
+    removeConsole: {
+      exclude: ["error"],
+    },
+  },
+};
 
 export default withNextIntl(nextConfig);
